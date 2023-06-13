98851851809fae760815f88c4dead7e0a2dea2ed
Dwwwarfff <peganovav@gmail.com>
2023-06-13 21:35:23 +0300

    Just a pattern

---
+++ Test.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestForTask6(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
